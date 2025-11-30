1cba0d7838da567893c9478a09829996e5eb2107
Avi BenEmanuel <avraham.benemanuel@gmail.com>
2025-11-30 12:13:40 +0200

    Rebuild Hebrew parser with updated book names

--- esm/lang/he.js
+++ esm/lang/he.js
@@ -182,8 +182,7 @@ var bcv_regexps = class {
         osis: ["Ps"],
         testament: "oa",
         testament_books: { "Ps": "oa" },
-        regexp:
-      /(?:^|(?<=[^\p{L}]))((?:מזמורים|תהילים|תהלים|Ps))(?:(?=[\d\s.:,;\x1e\x1f&\(\)（）\[\]\/"'\*=~\-–—])|$)/giu
+        regexp: /(?:^|(?<=[^\p{L}]))((?:מזמורים|תהילים|Ps))(?:(?=[\d\s.:,;\x1e\x1f&\(\)（）\[\]\/"'\*=~\-–—])|$)/giu
       },
       {
         osis: ["PrAzar"],
